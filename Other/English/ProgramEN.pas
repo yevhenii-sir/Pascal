9c80a4f59cab6caecc6454532103d23fc7d494ed
Yevhenii Sirenko <ew_sirenko@ukr.net>
2020-08-26 01:52:32 +0300

    Added a procedure for finding a file

---
+++ Other/English/ProgramEN.pas
@@ -0,0 +1,27 @@
+﻿Program English;
+uses crt;
+const word_count = 40;
+type mas = array[1..word_count] of string;
+var word_array: mas;
+    text_file: text;
+    string_line, file_name: string;
+    i: integer;
+
+Procedure search_file();
+begin
+Write('Take out the TXT file name: ');
+Readln(file_name);
+file_name := file_name + '.txt';
+While not fileexists(file_name) do begin
+  Writeln('File Not Found');
+  Write('Take out the TXT file name: ');
+Readln(file_name);
+file_name := file_name + '.txt';
+end;
+end;
+
+
+begin
+search_file;
+
+end.
