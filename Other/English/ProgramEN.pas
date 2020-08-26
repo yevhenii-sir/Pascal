b75b1552d3ee223ae9365061a6d1e70119b78ec2
Yevhenii Sirenko <ew_sirenko@ukr.net>
2020-08-26 03:50:53 +0300

    Many changes

--- Other/English/ProgramEN.pas
+++ Other/English/ProgramEN.pas
@@ -1,34 +1,42 @@
-﻿Program English;
-//uses crt;
-const word_count = 40;
-type mas = array[1..word_count] of string;
-     used = set of byte;
-var used_word: used;
-    word_array: mas;
-    i, count:integer;
-    string_line, file_name: string;
-    
+﻿program English;
+uses crt;
+const
+  word_count = 40;
 
-Procedure search_file();
+type
+  mas = array[1..word_count] of string;
+  used = set of byte;
+
+var
+  used_word: used;
+  word_array: mas;
+  i, count, mistake: integer;
+  string_line, file_name: string;
+
+
+procedure search_file();
 begin
-Write('Take out the TXT file name: ');
-Readln(file_name);
-file_name := file_name + '.txt';
-While not fileexists(file_name) do begin
-  Writeln('File Not Found');
   Write('Take out the TXT file name: ');
-Readln(file_name);
-file_name := file_name + '.txt';
-end;
+  Readln(file_name);
+  file_name := file_name + '.txt';
+  while not fileexists(file_name) do
+  begin
+    Writeln('File Not Found');
+    Write('Take out the TXT file name: ');
+    Readln(file_name);
+    file_name := file_name + '.txt';
+  end;
 end;
 
-Procedure file_in_mas;
-var text_file: text;
+procedure file_in_mas; {Wrapping lines from file to array}
+var
+  text_file: text;
 begin
   Assign(text_file, file_name);
   Reset(text_file);
   count := 1;
-  While not EOF(text_file) do begin
+  while not EOF(text_file) do
+  begin
     Readln(text_file, string_line);
     word_array[count] := string_line;
     Inc(count);
@@ -37,19 +45,46 @@ begin
   Close(text_file);
 end;
 
-Function randomize_word:byte;
-var num_rnd: byte;
+function randomize_word: byte; 
+var
+  num_rnd: byte;
 begin
   num_rnd := Random(count) + 1;
-  While ([num_rnd] * used_word) = [num_rnd] do begin
-  num_rnd := Random(count) + 1; 
+  while ([num_rnd] * used_word) = [num_rnd] do
+  begin
+    num_rnd := Random(count) + 1; 
   end;
   used_word := used_word + [num_rnd];
   Result := num_rnd;
 end;
 
+procedure test_go;
+var
+  num: byte;
+  check, word_z: string;
 begin
- Randomize;
- search_file;
- file_in_mas;
+  mistake := 0;
+  for var i := 1 to count do
+  begin
+    num := randomize_word;
+    Write(Copy(word_array[num], pos('=', word_array[num]) + 2, Length(word_array[num])), ' = ');
+    Readln(check);
+    word_z := Copy(word_array[num], 1, pos('=', word_array[num]) - 2);
+    if check <> word_z then begin
+      Writeln('You entered incorrectly!');
+      Writeln('Privileged answer: ', word_z);
+      Inc(mistake);
+    end;
+    Writeln('-------------------------');
+  end;
+end;
+
+begin
+  Randomize;
+  search_file;
+  file_in_mas;
+  test_go;
+  Writeln('Correct answers: ', count - mistake);
+  Writeln('Not the correct answers: ', mistake);
+  Readln;
 end.
